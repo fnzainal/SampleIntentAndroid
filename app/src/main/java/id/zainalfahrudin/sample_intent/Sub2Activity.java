ccb0f380fd4edca061b27c5de00929484cab2719
Zainal Fahrudin <fnzainal@gmail.com>
2016-10-16 18:31:37 +0700

    first commit

---
+++ app/src/main/java/id/zainalfahrudin/sample_intent/Sub2Activity.java
@@ -0,0 +1,27 @@
+package id.zainalfahrudin.sample_intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class Sub2Activity extends AppCompatActivity {
+    public static String KEY_DATA = "data";
+    private String receivedData = null;
+    private TextView txtData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub2);
+
+        txtData = (TextView)findViewById(R.id.txt_data);
+        receivedData = getIntent().getStringExtra(KEY_DATA);
+        if (receivedData!=null) {
+            txtData.setText(receivedData);
+        }else{
+            txtData.setText("(data kosong)");
+            Log.d("TAG", "onCreate: text null");
+        }
+    }
+}
